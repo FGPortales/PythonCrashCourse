0d5ba9c39cd935476d2c37e2c4c163d83e732a88
FGPortales <fportalesar@gmail.com>
2020-11-13 01:00:32 -0500

    practicing

--- Chapter_6/alien.py
+++ Chapter_6/alien.py
@@ -1,4 +1,29 @@
-alien_0 = {'color': 'green', 'points': 5}
+# alien_0 = {'color': 'green', 'points': 5}
+# alien_0 = {}  # Dictionary empty
 
-print(alien_0['color'])
-print(alien_0['points'])
+# **Printing values from keys**
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+# **Adding new key-value pairs**
+# print(alien_0)
+# alien_0['x-position'] = 0
+# alien_0['y-position'] = 25
+# print(alien_0)
+
+alien_0 = {'x-position': 0, 'y-position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x-position']}")
+
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien
+    x_increment = 3
+# The new position is the old position plus the increment.
+alien_0['x-position'] = alien_0['x-position'] + x_increment
+
+print(f"New position: {alien_0['x-position']}")
