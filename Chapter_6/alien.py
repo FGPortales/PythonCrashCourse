866e4c984a421cd15467794e20afea43ccf7a893
FGPortales <fportalesar@gmail.com>
2020-11-16 23:11:26 -0500

    [IMP]solving

--- Chapter_6/alien.py
+++ Chapter_6/alien.py
@@ -27,3 +27,12 @@ else:
 alien_0['x-position'] = alien_0['x-position'] + x_increment
 
 print(f"New position: {alien_0['x-position']}")
+
+
+# Removing key-value pairs
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
---
+++ Chapter_6/person.py
@@ -0,0 +1,9 @@
+information = {
+    'first_name': 'Carlos',
+    'last_name': 'Pérez',
+    'age': '24',
+    'city': 'city'
+}
+
+for v in information:
+    print('Key: {} y Value: {}'.format(v, information[v]))
